690b16d111e6215b102f2f0c5e573d486ba5e5ce
AsifBashir <56689372+Asif26@users.noreply.github.com>
2023-06-23 15:56:45 +0500

    Rendering list

--- counter/src/components/counterComp.jsx
+++ counter/src/components/counterComp.jsx
@@ -1,31 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {
-        count: 0,
-        
-    };
-    render() { 
-        
-        return (
-        <> 
-        
+  state = {
+    count: 0,
+    tag:['tage1','tage2','tage3','tage3']
+  };
+  render() {
+    return (
+      <>
         <span className={this.getBageClasses()}> {this.formatCount()} </span>
-         <button className='btn btn-secondary btn-sm'>Increment</button>
-         </> 
-         );
-
-       
-    }
-    getBageClasses() {
-        let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
-        return classes;
-    }
-    formatCount(){
-        const {count} = this.state;
-        return( count === 0 ? 'Zero' : count)
-    }
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+            {this.state.tag.map((tag, key) => <li key={`index-${key}`}>{tag}</li>)}
+        </ul>
+      </>
+    );
+  }
+  getBageClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
- 
+
 export default Counter;
