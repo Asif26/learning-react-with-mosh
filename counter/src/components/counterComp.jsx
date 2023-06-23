99312b528372e34cebab8fae0aa48b050d33bd21
AsifBashir <56689372+Asif26@users.noreply.github.com>
2023-06-23 13:34:00 +0500

    learned how to add css classe un react

---
+++ counter/src/components/counterComp.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class Counter extends Component {
+    state = {
+        count: 0,
+        
+    };
+    render() { 
+        return (
+        <> 
+        
+        <span style={{fontSize:50}} className='badge badge-primary m-2'> {this.formatCount()} </span>
+         <button className='btn btn-secondary btn-sm'>Increment</button>
+         </> 
+         );
+    }
+    formatCount(){
+        const {count} = this.state;
+        return( count === 0 ? 'Zero' : count)
+    }
+}
+ 
+export default Counter;
