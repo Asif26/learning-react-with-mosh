b816ac6830c74761ae74d52f14619c515c58f94d
AsifBashir <56689372+Asif26@users.noreply.github.com>
2023-06-23 19:50:41 +0500

    added event handler

--- counter/src/components/counterComp.jsx
+++ counter/src/components/counterComp.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tag: ["Hello","Hi","Janu",],
+    tag: ["Hello", "Hi", "Janu"],
   };
   IfHandeler() {
     if (this.state.tag.length === 0) return <p>"Their is no tag"</p>;
@@ -15,14 +15,24 @@ class Counter extends Component {
       </ul>
     );
   }
-  handleIncrement = ()=> {
-    this.setState({count: this.state.count+1})
-  }
+  handleIncrement = (product) => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
+  };
+//   doHandlerIncrement = ()=>{
+//     this.handleIncrement({id:1})
+//   }
   render() {
     return (
       <>
         <span className={this.getBageClasses()}> {this.formatCount()} </span>
-        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={() => 
+            this.handleIncrement({id:1})}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
 
         {this.IfHandeler()}
         {this.state.tag.length === 0 && "Please Input tag"}
